aac514475bbdb552ad2134dbf7e3b6a00a967612
arunkumar <weakcoder@gmail.com>
2014-10-29 23:02:52 +0530

    1

--- ExpenseHead/src/main/java/com/expensehead/form/LoginForm.java
+++ ExpenseHead/src/main/java/com/expensehead/form/LoginForm.java
@@ -1,32 +1,41 @@
 package com.expensehead.form;
 
-public class LoginForm {
-	String groupId;
-	String userName;
-	String password;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class LoginForm {
+	
+	@Size(min=2, max=15)
+	@NotEmpty
+	private String groupId;
+	
+	
+	private String userName;
+	
+	@Size(min=2, max=15)
+	@NotEmpty
+	private String password;
+	
 	public String getGroupId() {
 		return groupId;
 	}
-
 	public void setGroupId(String groupId) {
 		this.groupId = groupId;
 	}
-
-	public String getName() {
+	public String getUserName() {
 		return userName;
 	}
-
-	public void setName(String userName) {
+	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	
+	
 }
