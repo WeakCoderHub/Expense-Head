8e636236687b151a7c56a47f7e061de4b63f9e58
arunkumar <weakcoder@gmail.com>
2014-10-29 22:57:47 +0530

    1

--- ExpenseHead/src/main/java/com/expensehead/controller/AccountPageController.java
+++ ExpenseHead/src/main/java/com/expensehead/controller/AccountPageController.java
@@ -1,23 +1,81 @@
 package com.expensehead.controller;
 
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.expensehead.form.LoginForm;
+import com.expensehead.model.Group;
+import com.expensehead.model.User;
+import com.expensehead.service.GroupService;
 
 @Controller
 public class AccountPageController {
 
+	@Autowired
+	private GroupService groupService;
+	
 	@RequestMapping(value={"/welcome","/"},method=RequestMethod.GET)
 	public String getHomePage(Model model){
 		model.addAttribute("loginForm", new LoginForm());
-		return "login1";
+		return "addfine";
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping(value={"/getUsers"},method=RequestMethod.GET, produces="application/json")
+	public Set<String> getUsers(@RequestParam("groupName") String groupName){
+		Set<String> users = new HashSet<String>();
+		users = groupService.getUsers(groupName);
+		return users;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value={"/getUserList"},method=RequestMethod.GET, produces="application/json")
+	public Map<String,String> getUserList(@RequestParam("groupName") String groupName){
+		//Map<String,String> userList = groupService.getUsersList(groupName);
+		Group g = new Group();
+		g.setGroupName("Banana");
+		g.setNoOfMembers(2);
+		User u1 = new User();
+		User u2 = new User();
+		List<User> user = new ArrayList<User>();
+		 u1.setUserName("ankit");
+		u1.setEmailId("ankit010ankit@gmail.com");
+		
+		u2.setUserName("arun");
+		u2.setEmailId("arun@gmail.com");
+		user.add(u1);
+		user.add(u2);
+		g.setUsers(user);
+		groupService.createGroup(g);
+		
+		return null;
 	}
 	
 	@RequestMapping(value={"/login"},method=RequestMethod.POST)
-	public String login(Model model){
+	public String login(@Valid LoginForm loginForm,
+            BindingResult bindingResult,Model model){
+		
+		if (bindingResult.hasErrors()) {
+            return "addfine";
+        }
+		
 		return "admin";
 	}
 	
