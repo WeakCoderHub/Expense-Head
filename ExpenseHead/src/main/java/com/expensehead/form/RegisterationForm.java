bcf35e2bc339683f3cd8465b366492db59141a00
arunkumar <weakcoder@gmail.com>
2014-11-30 00:13:57 +0530

    Registeration Form

---
+++ ExpenseHead/src/main/java/com/expensehead/form/RegisterationForm.java
@@ -0,0 +1,60 @@
+package com.expensehead.form;
+
+public class RegisterationForm {
+
+	String groupName;
+	String address;
+	String userName;
+	String email;
+	String phoneNumber;
+	char userType;
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public char getUserType() {
+		return userType;
+	}
+
+	public void setUserType(char userType) {
+		this.userType = userType;
+	}
+
+}
