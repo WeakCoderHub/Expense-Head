a1557e2baae9a5b84d2fb4a66595e65945459fa8
Ankit Gupta <ankitguptatu@gmail.com>
2015-02-15 13:34:23 +0530

    Service

--- ExpenseHead/src/main/java/com/expensehead/service/TransactionService.java
+++ ExpenseHead/src/main/java/com/expensehead/service/TransactionService.java
@@ -2,10 +2,12 @@ package com.expensehead.service;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.expensehead.form.AddContributionForm;
 import com.expensehead.form.AddExpenseForm;
 
 public interface TransactionService {
 
 	public int addExpense(AddExpenseForm expenseForm,HttpServletRequest request);
+	public int addContribution(AddContributionForm contributionForm, HttpServletRequest request);
 	
 }
--- ExpenseHead/src/main/java/com/expensehead/service/impl/TransactionServiceImp.java
+++ ExpenseHead/src/main/java/com/expensehead/service/impl/TransactionServiceImp.java
@@ -9,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.expensehead.dao.TransactionDao;
+import com.expensehead.dao.UserDao;
+import com.expensehead.form.AddContributionForm;
 import com.expensehead.form.AddExpenseForm;
+import com.expensehead.model.Journal;
 import com.expensehead.model.Transactions;
+import com.expensehead.model.User;
 import com.expensehead.service.TransactionService;
 
 @Service
@@ -18,12 +22,49 @@ public class TransactionServiceImp implements TransactionService{
 
     @Autowired
     TransactionDao transactionDao;
+    
+    @Autowired
+    UserDao userDao;
 	
 	@Override
 	@Transactional
 	public int addExpense(AddExpenseForm expenseForm, HttpServletRequest request) {
-        Transactions transaction = new  Transactions();
-        int result ;
+		int result ;
+		Transactions transaction = populateTransaction(expenseForm, request);
+		User user = userDao.getUser(String.valueOf(transaction.getUserId()));
+		if("false".equalsIgnoreCase(expenseForm.getPool()))
+		{
+			if(user.getPayable()>=transaction.getAmount())
+			{
+				result = transactionDao.saveExpense(transaction,true);
+			}
+			else
+			{
+				result = transactionDao.saveExpense(transaction,false);
+			}
+		}
+		else
+		{		
+			result = transactionDao.savePoolExpense(transaction);
+		}
+		
+		
+		return result;
+	}
+
+	@Override
+	public int addContribution(AddContributionForm contributionForm,HttpServletRequest request) {
+		Journal journal = new Journal();
+		int userId = (Integer)request.getSession().getAttribute("userId");
+		journal.setUserId(userId);
+		journal.setRecordDate(new Date());
+		journal.setDebit(contributionForm.getContributionAmount());
+		return 1;
+	}
+	
+	public Transactions populateTransaction(AddExpenseForm expenseForm, HttpServletRequest request)
+	{
+		Transactions transaction = new  Transactions();
         int groupId= (Integer)request.getSession().getAttribute("groupId");
         int userId = (Integer)request.getSession().getAttribute("userId");
         int amount = Integer.parseInt(expenseForm.getAmount());
@@ -32,17 +73,10 @@ public class TransactionServiceImp implements TransactionService{
 		transaction.setExpenseType(expenseForm.getExpenseType());
 		transaction.setGroupId(groupId);
 		transaction.setPoolFlag((expenseForm.getPool().equalsIgnoreCase("true")?true:false));
-		transaction.setUserId((Integer)request.getSession().getAttribute("userId"));
+		transaction.setUserId(userId);
 		transaction.setDiscription(expenseForm.getDescription());
 		transaction.setEntryDate(new Date());
-		if(expenseForm.getPool().equalsIgnoreCase("true")){
-			result = transactionDao.savePoolExpense(transaction,amount ,groupId);
-		}else{
-			result = transactionDao.saveExpense(transaction,amount,groupId,userId);
-		}
-		
-		
-		return result;
+		return transaction;
 	}
 
 }
